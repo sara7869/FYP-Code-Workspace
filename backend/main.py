80fe04c6d3cc444031c76e128d92191ccbf36030
sara7869 <sarayusri17@gmail.com>
2023-12-26 14:00:14 +0530

    Loaded all 6 models

--- backend/main.py
+++ backend/main.py
@@ -6,26 +6,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-
-
-# libraries for models
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, RandomForestClassifier
-
 app = Flask(__name__)
 
 # Load model
-model = pickle.load(open('../Employee Analysis Attrition Report - Random Forest calssifier.pkl', 'rb'))
+random_forest_model = pickle.load(open('../Employee Analysis Attrition Report - Random Forest calssifier.pkl', 'rb'))
+bagging_classifier_model = pickle.load(open('../Employee Analysis Attrition Report - bagging classifier.pkl', 'rb'))
+adaboost_classifier_model = pickle.load(open('../Employee Analysis Attrition Report - Adaboost classifier.pkl', 'rb'))
+gradientboost_classifier_model = pickle.load(open('../Employee Analysis Attrition Report - Gradientboot classifier.pkl', 'rb'))
+knn_classifier_model = pickle.load(open('../Employee Analysis Attrition Report - KNN classifier.pkl', 'rb'))
+mlp_model = pickle.load(open('../Employee Analysis Attrition Report - MLP.pkl', 'rb'))
+
 
 # Input data
-input_data = {
+# Row 1100 - No
+input_data1 = {
     "Age": [40],
     "BusinessTravel": ['Non-Travel'],
     "DailyRate": [1142],
@@ -62,34 +56,91 @@ input_data = {
     "YearsWithCurrManager": [2]
 }
 
-input_data_df = pd.DataFrame(input_data)
+# Row 999 - Yes
+input_data2 = {
+    "Age": [27],
+    "BusinessTravel": ['Travel_Rarely'],
+    "DailyRate": [135],
+    "Department": ['Research & Development'],
+    "DistanceFromHome": [17],
+    "Education": [4],
+    "EducationField": ['Life Sciences'],
+    "EmployeeCount": [1],
+    "EmployeeNumber": [1405],
+    "EnvironmentSatisfaction": [4],
+    "Gender": ['Female'],
+    "HourlyRate": [51],
+    "JobInvolvement": [3],
+    "JobLevel": [2],
+    "JobRole": ['Research Scientist'],
+    "JobSatisfaction": [3],
+    "MaritalStatus": ['Single'],
+    "MonthlyIncome": [2394],
+    "MonthlyRate": [25681],
+    "NumCompaniesWorked": [1],
+    "Over18": ['Y'],
+    "OverTime": ['Yes'],
+    "PercentSalaryHike": [13],
+    "PerformanceRating": [3],
+    "RelationshipSatisfaction": [4],
+    "StandardHours": [80],
+    "StockOptionLevel": [0],
+    "TotalWorkingYears": [8],
+    "TrainingTimesLastYear": [2],
+    "WorkLifeBalance": [3],
+    "YearsAtCompany": [8],
+    "YearsInCurrentRole": [2],
+    "YearsSinceLastPromotion": [7],
+    "YearsWithCurrManager": [7]
+}
 
-# for index, tup in enumerate(algorithms):
-    # name = tup[0]
-    # algorithm = tup[1]
-    # model = predict(tup[0],tup[1])
-    # model_and_score[tup[0]] = str(model.score(X_train,y_train)*100)+"%"
-    # trained_models.append((tup[0],model))
+input_data_df1 = pd.DataFrame(input_data1)
+input_data_df2 = pd.DataFrame(input_data2)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
 
-@app.route("/predict")
-def predict():
-
-    # print(name+' Report :')
-#     pred = model.predict(X_test)
-    pred_testrow = model.predict(input_data_df)
-#     print(classification_report(y_test, pred))
-    # print("My row prediction: ", pred_testrow)
+@app.route("/predict/random-forest")
+def randomForest():
+    line = predict(random_forest_model)
+    return line
     
-    # Convert the NumPy array to a string
-    prediction_str = str(pred_testrow)
-
-    line: str = "<p>"+" Report: " + "My row prediction: " + prediction_str + "</p>"
-
+@app.route("/predict/bagging-classifier")
+def baggingClassifier():
+    line = predict(bagging_classifier_model)
     return line
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/predict/adaboost-classifier")
+def adaboostClassifier():
+    line = predict(adaboost_classifier_model)
+    return line
+
+@app.route("/predict/gradientboost-classifier")
+def gradientboosterClassifier():
+    line = predict(gradientboost_classifier_model)
+    return line
+
+@app.route("/predict/knn-classifier")
+def knnClassifier():
+    line = predict(knn_classifier_model)
+    return line
+
+@app.route("/predict/mlp")
+def mlp():
+    line = predict(mlp_model)
+    return line
+
+    
+def predict(model):
+    
+    pred_testrow1 = model.predict(input_data_df1)
+    pred_testrow2 = model.predict(input_data_df2)
+    
+    # Convert the NumPy array to a string
+    prediction_str1 = str(pred_testrow1)
+    prediction_str2 = str(pred_testrow2)
+
+    line: str = "<p>"+ "My 1st row prediction: Attrition: " + prediction_str1 +  "</p><p> My 2nd row prediction: Attrition: " + prediction_str2 + "</p>"
+
+    return line
