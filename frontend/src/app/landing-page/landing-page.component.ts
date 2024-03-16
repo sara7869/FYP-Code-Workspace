558fdac3773e93cbca94e8a8c0398ba53426c666
sara7869 <sarayusri17@gmail.com>
2024-03-16 11:42:56 +0530

    Created landing page

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  predictionsList: Array<string> = [
+    'Prediction 1',
+    'Prediction 2',
+    'Prediction 3',
+    'Prediction 4',
+    'Prediction 5',
+    'Prediction 6',
+    'Prediction 7',
+    'Prediction 8',
+    'Prediction 9',
+  ];
+
+  ngOnInit(): void {
+    // TODO: Get list of past predictions
+
+  }
+
+  onPredictClick(): void {
+    // TODO: Implement contacting API endpoint through an Angular service class
+  }
+
+  onCancelClick(): void {
+
+  }
 
 }
