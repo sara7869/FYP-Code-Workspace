688ccd590cc3b1738b0ea66a380ccd94e7f21134
sara7869 <sarayusri17@gmail.com>
2024-03-18 16:31:07 +0530

    Implemented frontend to backend request

--- backend/main.py
+++ backend/main.py
@@ -1,130 +1,45 @@
+# The code snippet for converting the job role value from the frontend form to a numerical value to get job level value will be done in line 15-17
+
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
+from flask import Flask, request
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Load models
 ensemble_model_stacking = pickle.load(open('../ensemble_model_stacking.pkl', 'rb'))
 ensemble_model_voting = pickle.load(open('../ensemble_model_voting.pkl', 'rb'))
 ensemble_model_simple_average = pickle.load(open('../ensemble_model_simple_average.pkl', 'rb'))
 
-# Input data
-# Row 1100 - No
-input_data1 = {
-    "Age": [40],
-    "BusinessTravel": ['Non-Travel'],
-    "DailyRate": [1142],
-    "Department": ['Research & Development'],
-    "DistanceFromHome": [8],
-    "Education": [2],
-    "EducationField": ['Life Sciences'],
-    "EmployeeCount": [1],
-    "EmployeeNumber": [1552],
-    "EnvironmentSatisfaction": [4],
-    "Gender": ['Male'],
-    "HourlyRate": [72],
-    "JobInvolvement": [3],
-    "JobLevel": [2],
-    "JobRole": ['Healthcare Representative'],
-    "JobSatisfaction": [4],
-    "MaritalStatus": ['Divorced'],
-    "MonthlyIncome": [4069],
-    "MonthlyRate": [8841],
-    "NumCompaniesWorked": [3],
-    "Over18": ['Y'],
-    "OverTime": ['Yes'],
-    "PercentSalaryHike": [18],
-    "PerformanceRating": [3],
-    "RelationshipSatisfaction": [3],
-    "StandardHours": [80],
-    "StockOptionLevel": [0],
-    "TotalWorkingYears": [8],
-    "TrainingTimesLastYear": [2],
-    "WorkLifeBalance": [3],
-    "YearsAtCompany": [2],
-    "YearsInCurrentRole": [2],
-    "YearsSinceLastPromotion": [2],
-    "YearsWithCurrManager": [2]
-}
 
-# Row 999 - Yes
-input_data2 = {
-    "Age": [27],
-    "BusinessTravel": ['Travel_Rarely'],
-    "DailyRate": [135],
-    "Department": ['Research & Development'],
-    "DistanceFromHome": [17],
-    "Education": [4],
-    "EducationField": ['Life Sciences'],
-    "EmployeeCount": [1],
-    "EmployeeNumber": [1405],
-    "EnvironmentSatisfaction": [4],
-    "Gender": ['Female'],
-    "HourlyRate": [51],
-    "JobInvolvement": [3],
-    "JobLevel": [2],
-    "JobRole": ['Research Scientist'],
-    "JobSatisfaction": [3],
-    "MaritalStatus": ['Single'],
-    "MonthlyIncome": [2394],
-    "MonthlyRate": [25681],
-    "NumCompaniesWorked": [1],
-    "Over18": ['Y'],
-    "OverTime": ['Yes'],
-    "PercentSalaryHike": [13],
-    "PerformanceRating": [3],
-    "RelationshipSatisfaction": [4],
-    "StandardHours": [80],
-    "StockOptionLevel": [0],
-    "TotalWorkingYears": [8],
-    "TrainingTimesLastYear": [2],
-    "WorkLifeBalance": [3],
-    "YearsAtCompany": [8],
-    "YearsInCurrentRole": [2],
-    "YearsSinceLastPromotion": [7],
-    "YearsWithCurrManager": [7]
-}
-
-input_data_df1 = pd.DataFrame(input_data1)
-input_data_df2 = pd.DataFrame(input_data2)
-
-def predict(model):
-    
-    pred_testrow1 = model.predict(input_data_df1)
-    pred_testrow2 = model.predict(input_data_df2)
-    
-    # Convert the NumPy array to a string
-    prediction_str1 = str(pred_testrow1)
-    prediction_str2 = str(pred_testrow2)
-
-    line: str = "<p>"+ "My 1st row prediction: Attrition: " + prediction_str1 +  "</p><p> My 2nd row prediction: Attrition: " + prediction_str2 + "</p>"
-
-    return line
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</p>"
-
-@app.route("/predict/ensemble-model-stacking") 
-def ensembleModelStacking():
-    # line = predict(ensemble_model_stacking)
-    line = "Test line for ensemble_model_stacking"
-    return line
-
-@app.route("/predict/ensemble-model-voting")
-def ensembleModelVoting():
-    line = predict(ensemble_model_voting)
-    return line
-
-@app.route("/predict/ensemble-model-simple-average")
-def ensembleModelSimpleAverage():
-    line = predict(ensemble_model_simple_average)
-    return line
+# Implement the backend API for predictions using all 3 models using form data from angular 
+# frontend
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    # Get the form data from the frontend
+    form_data = request.get_json()
+    print(form_data)
+    # Predict using the 3 models
+    prediction_stacking = ensemble_model_stacking.predict(form_data)
+    prediction_voting = ensemble_model_voting.predict(form_data)
+    prediction_simple_average = ensemble_model_simple_average.predict(form_data)
+    # Return the predictions to the frontend
+    return {
+        "prediction_stacking": prediction_stacking,
+        "prediction_voting": prediction_voting,
+        "prediction_simple_average": prediction_simple_average
+    }
+    # return form_data
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,62 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
+
 export class LandingPageComponent implements OnInit {
+
+  // Fields -	YearsWithCurrManager
+  myForm = this.formBuilder.group({
+    age: '',
+    business_travel: '',
+    daily_rate: '',
+    department: '',
+    distance_from_home: '',
+    education: '',
+    education_field: '',
+    employee_count: '1',
+    employee_number: '',
+    environment_satisfaction: '',
+    gender: '',
+    hourly_rate: '',
+    job_involvement: '',
+    job_level: '',
+    job_role: '',
+    job_satisfaction: '',
+    marital_status: '',
+    monthly_income: '',
+    monthly_rate: '',
+    num_companies_worked: '',
+    over_18: '',
+    over_time: '',
+    percent_salary_hike: '',
+    performance_rating: '',
+    relationship_satisfaction: '',
+    standard_hours: '',
+    stock_option_level: '',
+    total_working_years: '',
+    training_times_last_year: '',
+    work_life_balance: '',
+    years_at_company: '',
+    years_in_current_role: '',
+    years_since_last_promotion: '',
+    years_with_curr_manager: ''
+  });
+  selectedJobRole: string = '';
 
   predictionsList: Array<string> = [
     'Prediction 1',
@@ -24,17 +70,58 @@ export class LandingPageComponent implements OnInit {
     'Prediction 9',
   ];
 
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+  ) { }
+
   ngOnInit(): void {
     // TODO: Get list of past predictions
-
+    console.log("Page initialized");
   }
 
   onPredictClick(): void {
-    // TODO: Implement contacting API endpoint through an Angular service class
+    // TODO: Implement contacting flask API endpoint through an Angular service class
+    this.myForm.controls['employee_count'].setValue('1');
+    console.log(this.myForm.value);
+    this.http.post('http://localhost:5000/predict', this.myForm.value).subscribe((data: any) => {
+      console.log(data);
+      this.predictionsList = data.predictions;
+    });
   }
 
   onCancelClick(): void {
+    // Clear form fields
+    this.myForm.reset();
+  }
+
+  // When job role is selected, set job level accordingly
+  onJobRoleSelected(event: any): void {
+    // select job level element and assign value
+    this.selectedJobRole = event.target.value;
+    console.log("Selected job role:", this.selectedJobRole);
+
+    // Define a mapping of job roles to job levels
+    const jobLevelMapping: { [key: string]: number } = {
+      "Sales Representative": 1,
+      "Laboratory Technician": 1,
+      "Human Resources": 1,
+      "Research Scientist": 1,
+      "Healthcare Representative": 2,
+      "Sales Executive": 2,
+      "Manager": 3,
+      "Research Director": 3,
+      "Manufacturing Director": 4,
+      "Executive Director": 5,
+      "Director": 5
+    };
+
+    // Set the job level based on the selected job role
+    // Use a type assertion to tell TypeScript that jobLevelMapping[this.selectedJobRole] is a number
+    this.myForm.controls['job_level'].setValue(String(jobLevelMapping[this.selectedJobRole]));
+    console.log("Job level set to:", jobLevelMapping[this.selectedJobRole]);
 
   }
 
 }
+
